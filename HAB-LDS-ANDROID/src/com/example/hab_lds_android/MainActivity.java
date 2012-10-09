d4d13e971cc39cec4bd136087816ce88df5b4a11
Colton Sablinsky <csablinsky@gmail.com>
2012-10-09 18:22:26 -0400

    Initial GUI Completed

--- HAB-LDS-ANDROID/src/com/example/hab_lds_android/MainActivity.java
+++ HAB-LDS-ANDROID/src/com/example/hab_lds_android/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.hab_lds_android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main2);
+        
+        Spinner select=(Spinner) findViewById(R.id.spinner1);
+        
+        ArrayAdapter <CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.polls, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        select.setAdapter(adapter);
     }
 
     @Override
