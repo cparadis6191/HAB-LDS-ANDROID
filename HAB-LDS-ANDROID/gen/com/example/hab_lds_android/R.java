3c0fcd26a5b33359e858f92e4fda7886fb073e37
Colton Sablinsky <csablinsky@gmail.com>
2012-10-11 10:31:17 -0400

    Button listener and basic Bluetooth framework set up.

--- HAB-LDS-ANDROID/gen/com/example/hab_lds_android/R.java
+++ HAB-LDS-ANDROID/gen/com/example/hab_lds_android/R.java
@@ -18,12 +18,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
         public static final int menu_settings=0x7f080005;
+        public static final int sendbutton=0x7f080001;
         public static final int spinner1=0x7f080000;
-        public static final int spinner2=0x7f080003;
-        public static final int textView1=0x7f080004;
+        public static final int spinner2=0x7f080004;
+        public static final int textView1=0x7f080002;
+        public static final int viewbutton=0x7f080003;
     }
     public static final class layout {
         public static final int main2=0x7f030000;
